c7a46ce289e2ffac6caf69e30508ae8319b5d73b
kaklakariada <kaklakariada@users.noreply.github.com>
2015-07-31 15:23:23 +0200

    Added failing integration test

--- test/itest.js
+++ test/itest.js
@@ -3,6 +3,7 @@
 'use strict';
 var gulp = require('gulp');
 var should = require('should');
+var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 var gutil = require('gulp-util');
@@ -10,13 +11,34 @@ var through = require('through2');
 
 var couchapp = require('../index');
 
+
+function getFile(filePath) {
+    console.log(__dirname);
+    return new gutil.File({
+        path:     filePath,
+        cwd:      __dirname,
+        base:     path.dirname(filePath),
+        contents: fs.readFileSync(filePath)
+    });
+}
+
+function getExpected(filePath) {
+    return getFile(path.join('test', 'expected', filePath));
+}
+
+function compareJsonContent(expectedFile, actualFile) {
+  var expectedString = String(expectedFile.contents)
+  var actualString = String(actualFile.contents)
+  assert.deepEqual(JSON.parse(actualString), JSON.parse(expectedString));
+}
+
 describe('couchapp()', function() {
     it('should build couchapp document from app', function(done) {
       gulp.task('testCouchAppTask', function(cb) {
         gulp.src(['./test/fixtures/test-couchapp/**'])
           .pipe(couchapp())
           .pipe(through.obj(function(file, enc, cb) {
-            console.log("file: ", file);
+            compareJsonContent(getExpected('test-couchapp-raw.json'), file);
             done();
             cb();
           }));
